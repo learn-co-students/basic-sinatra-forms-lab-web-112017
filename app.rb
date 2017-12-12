a85ca6214aff2b6024abf5fc762bf8a453ea4ed6
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2017-12-12 13:42:46 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    @nameparams = params["name"]
+    @name = params["name"]
     @coach = params["coach"]
     @pg = params["pg"]
     @sg = params["sg"]
